e802870ed8ef1bc69187808b48c401c3eb1101bf
Cvqtko <cvqtkokirilov@gmail.com>
2020-04-03 14:38:54 +0300

    Solutions for data types exercises

---
+++ 05_data_types_and_variables_exercise/src/Exercise_03.java
@@ -0,0 +1,9 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Exercise_03 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+	}
+}
