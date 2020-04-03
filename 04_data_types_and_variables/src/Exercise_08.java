cb2e9edaa7b520fe18d5c5d88bcc958ae4038b63
Cvqtko <cvqtkokirilov@gmail.com>
2020-04-03 14:19:39 +0300

    Refactoring of exercise

--- 04_data_types_and_variables/src/Exercise_08.java
+++ 04_data_types_and_variables/src/Exercise_08.java
@@ -5,7 +5,7 @@ public class Exercise_08 {
 		Scanner scanner = new Scanner(System.in);
 		char firstChar = scanner.nextLine().charAt(0);
 		
-		if(Character.toUpperCase(firstChar) == firstChar) {
+		if(Character.isUpperCase(firstChar)) {
 			System.out.println("upper-case");
 		}else {
 			System.out.println("lower-case");
