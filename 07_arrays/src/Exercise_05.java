a4ccaf329f40e3ae823062ef4f6cf1606ba77975
Cvqtko <cvqtkokirilov@gmail.com>
2020-04-04 17:36:38 +0300

    Solutions for exercises from arrays

---
+++ 07_arrays/src/Exercise_05.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Exercise_05 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int[] numbers = Arrays
+				.stream(scanner.nextLine().split(" "))
+				.mapToInt(e -> Integer.parseInt(e))
+				.toArray();
+		int sumEven = 0;
+		int sumOdd = 0;
+		
+		for (int i = 0; i < numbers.length; i++) {
+			if(numbers[i]%2==0) {
+				sumEven+=numbers[i];
+			}else {
+				sumOdd+=numbers[i];
+			}
+		}
+		System.out.println(sumEven-sumOdd);
+	}
+}
