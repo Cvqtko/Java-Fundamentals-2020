e00bb3e79ee73ce7836003d060f63b371058049f
Cvqtko <cvqtkokirilov@gmail.com>
2020-04-06 19:54:36 +0300

    Solutions for exercises from lists

---
+++ 12_lists/src/Exercise_04.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Exercise_04 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt)
+				.collect(Collectors.toList());
+
+		while (true) {
+			String line = scanner.nextLine();
+			if ("end".equals(line)) {
+				break;
+			}
+
+			String[] tokens = line.split("\\s+");
+
+			switch (tokens[0]) {
+			case "Add":
+				int numberToAdd = Integer.parseInt(tokens[1]);
+				numbers.add(numberToAdd);
+				break;
+			case "Remove":
+				int numberToReove = Integer.parseInt(tokens[1]);
+				numbers.remove(numberToReove);
+				break;
+			case "RemoveAt":
+				int indexToRemove = Integer.parseInt(tokens[1]);
+				numbers.remove(indexToRemove);
+				break;
+			case "Insert":
+				int numberToInsert = Integer.parseInt(tokens[1]);
+				int indexToInsert = Integer.parseInt(tokens[2]);
+				numbers.add(indexToInsert, numberToInsert);
+				break;
+			}
+		}
+		System.out.println(numbers.stream().map(Object::toString)
+                .collect(Collectors.joining(" ")));
+	}
+}
