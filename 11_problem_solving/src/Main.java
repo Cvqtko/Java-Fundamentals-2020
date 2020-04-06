d2a0dce7f03968512cf61b9c6c4911eb24dafe91
Cvqtko <cvqtkokirilov@gmail.com>
2020-04-06 15:45:11 +0300

    Bulls and Cows exercise from problem solving

---
+++ 11_problem_solving/src/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Initializes a JOptionPane and runs BCGuesser
+ */
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    public static void main (String[] args)
+    {
+        BullsAndCows app = new BullsAndCows();
+        app.createAppForm();
+
+        app.setVisible(true);
+
+    }
+
+}
+
