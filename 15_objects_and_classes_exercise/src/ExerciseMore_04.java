0e24652bc7992aded3b21c25fce30ba4fbb46129
Cvqtko <cvqtkokirilov@gmail.com>
2020-04-08 20:38:25 +0300

    More exercises on classes

---
+++ 15_objects_and_classes_exercise/src/ExerciseMore_04.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ExerciseMore_04 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+	}
+
+}
---
+++ 15_objects_and_classes_exercise/src/Exercise_07.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercise_07 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+	}
+
+}
