df440666941e37a2a571b9b717761dd3e9149c08
Cvqtko <cvqtkokirilov@gmail.com>
2020-04-08 16:02:50 +0300

    Exercises on objects and classes

---
+++ 14_objects_and_classes/src/Exercise_01.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercise_01 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		String[] words = scanner.nextLine().split(" ");
+
+		Random rnd = new Random();
+
+		for (int i = 0; i < words.length; i++) {
+			int swapIndex = rnd.nextInt(words.length);
+			String temp = words[i];
+			words[i] = words[swapIndex];
+			words[swapIndex] = temp;
+		}
+		System.out.println(Arrays.toString(words));
+	}
+
+}
