50c9dfdbab54215c086f24313e5bd7940c95b714
Cvqtko <cvqtkokirilov@gmail.com>
2020-04-13 15:30:52 +0300

    Exam preparation exercises

--- 16_exam_preparation/src/Exercise_04.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Exercise_04 {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-
-		int n = Integer.parseInt(scanner.nextLine());
-	}
-}
--- 16_exam_preparation/src/TreasureHunt.java
+++ 16_exam_preparation/src/TreasureHunt.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-public class Exercise_02 {
+public class TreasureHunt {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		String[] split = scanner.nextLine().split("\\|");
@@ -33,7 +33,7 @@ public class Exercise_02 {
 				break;
 			case "Steal":
 				int count = Integer.parseInt(tokens[1]);
-				count = count > chest.size() ? chest.size() : count;
+				count = Math.min(count, chest.size());
 				List<String> stolen = new ArrayList<String>();
 				for (int i = 0; i < count; i++) {
 					int lastIndex = chest.size() - 1;
