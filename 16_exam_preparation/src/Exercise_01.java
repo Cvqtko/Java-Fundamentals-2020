71f50377b5c9e068590e8ebde1c94081be1647ce
Cvqtko <cvqtkokirilov@gmail.com>
2020-04-13 13:39:14 +0300

    Exam preparation exercises

---
+++ 16_exam_preparation/src/Exercise_01.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Exercise_01 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int days = Integer.parseInt(scanner.nextLine());
+		int plunderPerDay = Integer.parseInt(scanner.nextLine());
+		double targetPlunder = Double.parseDouble(scanner.nextLine());
+
+		double totalPlunder = 0;
+
+		for (int i = 1; i <= days; i++) {
+
+			totalPlunder += plunderPerDay;
+
+			if (i % 3 == 0) {
+				totalPlunder += plunderPerDay / 2;
+			}
+			if (i % 5 == 0) {
+				totalPlunder = 0.7 * totalPlunder;
+			}
+		}
+		if (totalPlunder >= targetPlunder) {
+			System.out.printf("Ahoy! %.2f plunder gained.", totalPlunder);
+		} else {
+			double collectedPercentage = (totalPlunder / targetPlunder) * 100;
+			System.out.printf("Collected only %.2f%% of the plunder.", collectedPercentage);
+		}
+	}
+}
---
+++ 16_exam_preparation/src/Exercise_03.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Exercise_03 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int n = Integer.parseInt(scanner.nextLine());
+	}
+}
