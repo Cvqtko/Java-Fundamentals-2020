8f0e367ec40e47046db59602934a5b096be36ae4
Cvqtko <cvqtkokirilov@gmail.com>
2020-04-16 14:16:42 +0300

    More exercises on associative arrays

---
+++ 18_associative_arrays_exercise/src/ExerciseMore_04.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class ExerciseMore_04 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+	}
+}
