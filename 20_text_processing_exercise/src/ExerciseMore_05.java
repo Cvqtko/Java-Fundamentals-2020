ac04536114f66f026563c1ecb0d889b1c0716458
Cvqtko <cvqtkokirilov@gmail.com>
2020-04-17 18:37:49 +0300

    More exercises on text processing

--- 20_text_processing_exercise/src/ExerciseMore_05.java
+++ 20_text_processing_exercise/src/ExerciseMore_05.java
@@ -3,6 +3,21 @@ import java.util.Scanner;
 public class ExerciseMore_05 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
+		StringBuilder article = new StringBuilder();
+		String header = "<h1>\n\t" + scanner.nextLine() + "\n</h1>";
+
+		String articleContent = "<article>\n\t" + scanner.nextLine() + "\n</article>";
+		article.append(header).append(System.lineSeparator());
+		article.append(articleContent).append(System.lineSeparator());
+		String line = scanner.nextLine();
+		while (!"end of comments".equals(line)) {
+			String comment = "<div>\n\t" + line + "\n</div>";
+			article.append(comment);
+			line = scanner.nextLine();
+			if (!"end of comments".equals(line)) {
+				article.append(System.lineSeparator());
+			}
+		}
+		System.out.println(article);
 	}
 }
