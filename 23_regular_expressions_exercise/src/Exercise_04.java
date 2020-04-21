c3640ee59ba4806681138104b31679e9b71700dd
Cvqtko <cvqtkokirilov@gmail.com>
2020-04-21 15:39:48 +0300

    Regular expressions exercises

--- 23_regular_expressions_exercise/src/Exercise_04.java
+++ 23_regular_expressions_exercise/src/Exercise_04.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -5,14 +7,46 @@ import java.util.regex.Pattern;
 public class Exercise_04 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		String regex = "\\b[A-Z][a-z]+ [A-Z][a-z]+\\b";
-		String input = scanner.nextLine();
 
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(input);
+		int n = Integer.parseInt(scanner.nextLine());
 
-		while (matcher.find()) {
-			System.out.println(matcher.group() + " ");
+		List<String> attackedPlanets = new LinkedList<>();
+		List<String> destroyedPlanets = new LinkedList<>();
+		while (n-- > 0) {
+			String regex = "[STARstar]";
+			String input = scanner.nextLine();
+			Pattern pattern = Pattern.compile(regex);
+			Matcher matcher = pattern.matcher(input);
+			int decryptionKey = 0;
+			while (matcher.find()) {
+				decryptionKey++;
+			}
+			StringBuilder decrytedMessage = new StringBuilder();
+
+			for (int i = 0; i < input.length(); i++) {
+				decrytedMessage.append((char) (input.charAt(i) - decryptionKey));
+			}
+
+			String planetRegex = "@(?<name>[A-Za-z]+)([^@:!\\->]*):(?<population>[0-9]+)([^@:!\\->]*)!(?<type>(A|D))!([^@:!\\->]*)->(?<count>[0-9]+)";
+			pattern = Pattern.compile(planetRegex);
+			matcher = pattern.matcher(decrytedMessage.toString());
+
+			while (matcher.find()) {
+
+				String name = matcher.group("name");
+				String type = matcher.group("type");
+
+				if ("A".equals(type)) {
+					attackedPlanets.add(name);
+				} else {
+					destroyedPlanets.add(name);
+				}
+			}
+
 		}
+		System.out.println("Attacked planets: " + attackedPlanets.size());
+		attackedPlanets.stream().forEach(p -> System.out.println("-> " + p));
+		System.out.println("Destroyed planets: " + destroyedPlanets.size());
+		destroyedPlanets.stream().forEach(p -> System.out.println("-> " + p));
 	}
 }
