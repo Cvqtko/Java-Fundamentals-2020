2ba4025e07db560debca145195e536a90c6c84b3
Cvqtko <cvqtkokirilov@gmail.com>
2021-04-18 20:02:05 +0300

    Exercises

--- 19_text_processing/src/Exercise_01.java
+++ 19_text_processing/src/Exercise_01.java
@@ -1,16 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Exercise_01 {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		String word = scanner.nextLine();
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String word = reader.readLine();
 
 		while (!"end".equals(word)) {
 			String reversed = reverse(word);
 
 			System.out.println(word + " = " + reversed);
 
-			word = scanner.nextLine();
+			word = reader.readLine();
 		}
 
 	}
